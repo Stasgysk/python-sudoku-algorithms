c37062a923e6146d2b7778b8dbc48cf58f254b8c
sg823pm <stanislav.guskov@student.tuke.sk>
2023-12-02 20:32:56 +0100

    4x4 implementation

--- src/sudoku/goose/filip/main/main.py
+++ src/sudoku/goose/filip/main/main.py
@@ -29,17 +29,17 @@ sudoku_field_generated_positions = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-sudoku_field_9_x_9_static = [
-    [0, 0, 4, 0, 6, 0, 0, 0, 5],
-    [7, 8, 0, 4, 0, 0, 0, 2, 0],
-    [0, 0, 2, 6, 0, 1, 0, 7, 8],
-    [6, 1, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 7, 5, 4, 0, 0, 6, 1],
-    [0, 0, 1, 7, 5, 0, 9, 3, 0],
-    [0, 7, 0, 3, 0, 0, 0, 1, 0],
-    [0, 4, 0, 2, 0, 6, 0, 0, 7],
-    [0, 2, 0, 0, 0, 7, 4, 0, 0],
-]
+# sudoku_field_9_x_9_static = [
+#     [0, 0, 4, 0, 6, 0, 0, 0, 5],
+#     [7, 8, 0, 4, 0, 0, 0, 2, 0],
+#     [0, 0, 2, 6, 0, 1, 0, 7, 8],
+#     [6, 1, 0, 0, 7, 5, 0, 0, 9],
+#     [0, 0, 7, 5, 4, 0, 0, 6, 1],
+#     [0, 0, 1, 7, 5, 0, 9, 3, 0],
+#     [0, 7, 0, 3, 0, 0, 0, 1, 0],
+#     [0, 4, 0, 2, 0, 6, 0, 0, 7],
+#     [0, 2, 0, 0, 0, 7, 4, 0, 0],
+# ]
 
 sudoku_field_4_x_4 = [
     [0, 0, 0, 0],
@@ -51,13 +51,15 @@ sudoku_field_4_x_4 = [
 font = pygame.font.SysFont("comicsans", 40)
 font1 = pygame.font.SysFont("comicsans", 20)
 diff = 500 / 9
-value = 0
+field_size = 9
+
 
 def clear_field(field):
-    for i in range(9):
-        for j in range(9):
+    for i in range(field_size):
+        for j in range(field_size):
             field[i][j] = 0
     return field
+
 
 # def check_if_field_solved(field):
 #     print("some shit")
@@ -70,47 +72,72 @@ def draw_box(x, y):
 
 
 def show_field(field):
-    for i in range(9):
-        for j in range(9):
+    for i in range(field_size):
+        for j in range(field_size):
             pygame.draw.rect(Window, (255, 255, 0), (i * diff, j * diff, diff + 1, diff + 1))
             if field[i][j] != 0:
                 text1 = font.render(str(field[i][j]), 1, (255, 0, 0))
                 Window.blit(text1, (i * diff + 15, j * diff))
-    for i in range(10):
-        if i % 3 == 0:
-            thick = 7
-        else:
-            thick = 1
-        pygame.draw.line(Window, (0, 0, 0), (0, i * diff), (500, i * diff), thick)
-        pygame.draw.line(Window, (0, 0, 0), (i * diff, 0), (i * diff, 500), thick)
+    if field_size == 9:
+        for i in range(10):
+            if i % 3 == 0:
+                thick = 7
+            else:
+                thick = 1
+            pygame.draw.line(Window, (0, 0, 0), (0, i * diff), (500, i * diff), thick)
+            pygame.draw.line(Window, (0, 0, 0), (i * diff, 0), (i * diff, 500), thick)
+    else:
+        for i in range(4):
+            if i % 2 == 0:
+                thick = 7
+            else:
+                thick = 1
+            pygame.draw.line(Window, (0, 0, 0), (0, i * diff), (500, i * diff), thick)
+            pygame.draw.line(Window, (0, 0, 0), (i * diff, 0), (i * diff, 500), thick)
 
 
 def is_input_valid(x, y, field, val):
-    for i in range(9):
+    for i in range(field_size):
         if field[x][i] == val:
             return False
         if field[i][y] == val:
             return False
-    xx = x // 3
-    yy = y // 3
-    start_x = xx * 3
-    start_y = yy * 3
-    for pos_x in range(start_x, start_x + 3):
-        for pos_y in range(start_y, start_y + 3):
+    if field_size == 9:
+        xx = x // 3
+        yy = y // 3
+        start_x = xx * 3
+        start_y = yy * 3
+        start_x_plus = start_x + 3
+        start_y_plus = start_y + 3
+    else:
+        xx = x // 2
+        yy = y // 2
+        start_x = xx * 2
+        start_y = yy * 2
+        start_x_plus = start_x + 2
+        start_y_plus = start_y + 2
+    for pos_x in range(start_x, start_x_plus):
+        for pos_y in range(start_y, start_y_plus):
             if field[pos_x][pos_y] == val:
                 return False
     return True
 
 
 def is_solvable(field):
-    for i in range(9):
-        for j in range(9):
+    for i in range(field_size):
+        for j in range(field_size):
             if field[i][j] != 0:
                 solvable = False
-                for val in range(1, 8):
-                    if is_input_valid(i, j, field, val):
-                        solvable = True
-                        break
+                if field_size == 9:
+                    for val in range(1, 9):
+                        if is_input_valid(i, j, field, val):
+                            solvable = True
+                            break
+                else:
+                    for val in range(1, 4):
+                        if is_input_valid(i, j, field, val):
+                            solvable = True
+                            break
                 if not solvable:
                     return solvable
     return True
@@ -118,16 +145,27 @@ def is_solvable(field):
 
 def generate_field(field):
     clear_field(field)
-    count = random.randint(30, 50)
+    print(field_size)
+    if field_size == 9:
+        count = random.randint(30, 50)
+    else:
+        count = 5
     for i in range(count):
         flag = True
         while flag:
-            x = random.randint(0, 8)
-            y = random.randint(0, 8)
+            if field_size == 9:
+                x = random.randint(0, 8)
+                y = random.randint(0, 8)
+            else:
+                x = random.randint(0, 3)
+                y = random.randint(0, 3)
             if field[x][y] != 0:
                 continue
             else:
-                val = random.randint(1, 9)
+                if field_size == 9:
+                    val = random.randint(1, 9)
+                else:
+                    val = random.randint(1, 4)
                 if is_input_valid(x, y, field, val):
                     if not is_solvable(field):
                         clear_field(field)
@@ -139,8 +177,8 @@ def generate_field(field):
 
 
 def is_solved(field):
-    for i in range(9):
-        for j in range(9):
+    for i in range(field_size):
+        for j in range(field_size):
             if field[i][j] == 0:
                 return False
     return True
@@ -200,6 +238,8 @@ def play_game(field):
                         field[x][y] = 0
                     case pygame.K_RETURN:
                         generate_field(field)
+        if field_size != 9 and val >= 5:
+            val = 0
         if is_input_valid(x, y, field, val) and sudoku_field_generated_positions[x][y] != 1:
             field[x][y] = val
         show_field(field)
@@ -211,6 +251,15 @@ def play_game(field):
 
 
 def main():
+    field_type = int(input("Choose your game field 1 or 2:\n1: 9x9\n2: 4x4\n"))
+    while field_type != 1 and field_type != 2:
+        print(field_type)
+        field_type = int(input("Choose your game field 1 or 2:\n1: 9x9\n2: 4x4\n"))
+    if field_type == 2:
+        global diff
+        diff= 500 / 4
+        global field_size
+        field_size = 4
     field = sudoku_field_9_x_9
     Window.fill((255, 182, 193))
     generate_field(field)
