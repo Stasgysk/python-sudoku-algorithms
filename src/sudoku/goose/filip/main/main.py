22f8c1201012eddcabb9e3122919bb1b1444951d
sg823pm <stanislav.guskov@student.tuke.sk>
2023-12-02 17:12:17 +0100

    init

---
+++ src/sudoku/goose/filip/main/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Starting game...")
+    print("Generating sudoku field...")
+    print("Trying to solve...")
+    print("Some...")
+
+
+if __name__ == "__main__":
+    main()
