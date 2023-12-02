5436388cdad08d2e8155e5e5382baebf044edbb5
sg823pm <stanislav.guskov@student.tuke.sk>
2023-12-02 17:17:25 +0100

    added functions

--- src/sudoku/goose/filip/main/main.py
+++ src/sudoku/goose/filip/main/main.py
@@ -7,3 +7,23 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+def check_if_field_solved(field):
+    print("some shit")
+
+
+def generate_field():
+    print("some shit")
+
+
+def solve_field():
+    print("some shit")
+
+
+def dfs():
+    print("some shit")
+
+
+def play_game():
+    print("some shit")
