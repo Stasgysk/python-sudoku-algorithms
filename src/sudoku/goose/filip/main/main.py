80b3c5d2a785ea26b28c9066a7e91fc6c997b8c9
sg823pm <stanislav.guskov@student.tuke.sk>
2023-12-02 17:44:15 +0100

    game field drawing

--- src/sudoku/goose/filip/main/main.py
+++ src/sudoku/goose/filip/main/main.py
@@ -26,7 +26,7 @@ sudoku_field_9_x_9_static = [
     [0, 7, 0, 3, 0, 0, 0, 1, 0],
     [0, 4, 0, 2, 0, 6, 0, 0, 7],
     [0, 2, 0, 0, 0, 7, 4, 0, 0],
-    ]
+]
 
 sudoku_field_4_x_4 = [
     [0, 0, 0, 0],
@@ -35,16 +35,12 @@ sudoku_field_4_x_4 = [
     [0, 0, 0, 0]
 ]
 
-
-def main():
-    print("Starting game...")
-    print("Generating sudoku field...")
-    print("Trying to solve...")
-    print("Some...")
-
-
-if __name__ == "__main__":
-    main()
+font = pygame.font.SysFont("comicsans", 40)
+font1 = pygame.font.SysFont("comicsans", 20)
+x = 0
+z = 0
+diff = 500 / 9
+value = 0
 
 
 def check_if_field_solved(field):
@@ -65,3 +61,35 @@ def dfs():
 
 def play_game():
     print("some shit")
+
+
+def show_field(field):
+    for i in range(9):
+        for j in range(9):
+            pygame.draw.rect(Window, (255, 255, 0), (i * diff, j * diff, diff + 1, diff + 1))
+            text1 = font.render(str(field[i][j]), 1, (255, 0, 0))
+            Window.blit(text1, (i * diff + 15, j * diff))
+    for i in range(10):
+        if i % 3 == 0:
+            thick = 7
+        else:
+            thick = 1
+        pygame.draw.line(Window, (0, 0, 0), (0, i * diff), (500, i * diff), thick)
+        pygame.draw.line(Window, (0, 0, 0), (i * diff, 0), (i * diff, 500), thick)
+
+
+def main():
+    Window.fill((255, 182, 193))
+    pygame.event.pump()
+    show_field(sudoku_field_9_x_9_static)
+    pygame.display.update()
+    print("Starting game...")
+    print("Generating sudoku field...")
+    print("Trying to solve...")
+    print("Some...")
+
+    input('Press Enter to exit')
+
+
+if __name__ == "__main__":
+    main()
